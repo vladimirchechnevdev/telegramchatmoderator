8639bfb426387a6278a51fd8ce8af89aa8bc1bc1
vladimirchechnev <vladimirchechnev121@gmail.com>
2025-11-14 08:42:43 +0700

    Translated everything into English.

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ bot = telebot.TeleBot(token)
 def response_get(response, message):
     connection = sqlite3.connect('UsersDatabase.sqlite')
     cursor = connection.cursor()
+
     if response == 0:
         bot.send_message(chat_id=message.chat.id,
                          text=message.from_user.first_name + ' ' + warnings[0])
@@ -26,7 +27,9 @@ def response_get(response, message):
             [message.from_user.first_name])
         connection.commit()
         connection.close()
+
         return None
+
     elif response == 1:
         bot.send_message(chat_id=message.chat.id,
                          text=message.from_user.first_name + ' ' + warnings[1])
@@ -39,13 +42,17 @@ def response_get(response, message):
             [message.from_user.first_name])
         connection.commit()
         connection.close()
+
         return None
+
     elif response == 2:
         bot.send_message(chat_id=message.chat.id,
                          text=message.from_user.first_name + ' ' + warnings[2])
         bot.ban_chat_member(chat_id=message.chat.id, user_id=message.from_user.id)
         connection.close()
+
         return None
+
     else:
         bot.send_message(chat_id=message.chat.id,
                          text=message.from_user.first_name + ' ' + warnings[0])
@@ -53,18 +60,22 @@ def response_get(response, message):
                        [message.from_user.first_name])
         connection.commit()
         connection.close()
+
         return None
 
 
 #checks user
 def check_user(message):
     username = message.from_user.first_name
+
     connection = sqlite3.connect('UsersDatabase.sqlite')
     cursor = connection.cursor()
     cursor.execute(f'SELECT * FROM [{message.chat.id}]')
+
     everything = cursor.fetchall()
     connection.close()
     flag = False
+
     if type(everything) == list:
         for i in range(len(everything)):
             for j in range(0, 3):
@@ -73,8 +84,10 @@ def check_user(message):
                     return j
                 else:
                     continue
+
     if not flag:
         return None
+
     if everything != list:
         return None
 
@@ -84,23 +97,29 @@ def check_table(message):
     connection = sqlite3.connect('UsersDatabase.sqlite')
     cursor = connection.cursor()
     cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+
     response = cursor.fetchall()
     flag = False
+
     for i in range(len(response)):
         if str(message.chat.id) == str(response[i][0]):
             flag = True
             break
         else:
             continue
+
     if not flag:
         cursor.execute(
             f"CREATE TABLE [{message.chat.id}] (first  TEXT, second TEXT, third  TEXT, options TEXT, name TEXT);")
         cursor.execute(f"INSERT INTO [{message.chat.id}] (options, name) VALUES (?, ?)", ["False", "Бот"])
         connection.commit()
         connection.close()
+
         return 'False'
+
     else:
         cursor.execute(f"SELECT options FROM [{message.chat.id}]")
+
         return cursor.fetchall()[0][0]
 
 #advertisement
@@ -108,14 +127,19 @@ def advertisement(message):
     connection = sqlite3.connect('UsersDatabase.sqlite')
     cursor = connection.cursor()
     response_database = cursor.execute("SELECT name FROM sqlite_master WHERE type='table';").fetchall()
+
     for i in range(len(response_database)):
         try:
             bot.send_message(chat_id=response_database[i][0], text=message.text)
+
         except telebot.apihelper.ApiTelegramException as err:
             response = str(err)[91:]
+
             if 'chat not found' in response:
                 continue
+
     connection.close()
+
     return None
 
 
@@ -124,6 +148,7 @@ def start_message(message):
     group_status = bot.get_chat(chat_id=message.chat.id).type
     connection = sqlite3.connect('UsersDatabase.sqlite')
     cursor = connection.cursor()
+
     if group_status == 'group' or group_status == 'supergroup':
         bot.send_message(chat_id=message.chat.id,
                          text='This bot was created to moderate groups. \n/start shows this message; \n/ban turns on / off '
@@ -132,18 +157,21 @@ def start_message(message):
         cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
         response = cursor.fetchall()
         flag = False
+
         for i in range(len(response)):
             if id == str(response[i][0]):
                 flag = True
                 break
             else:
                 continue
+
         if not flag:
             cursor.execute(
                 f"CREATE TABLE [{message.chat.id}] (first  TEXT, second TEXT, third  TEXT, options TEXT, name TEXT);")
             cursor.execute(f"INSERT INTO [{message.chat.id}] (options, name) VALUES (?, ?)", ["False", "Бот"])
             connection.commit()
             connection.close()
+
             return None
     else:
         bot.send_message(chat_id=message.chat.id,
@@ -152,8 +180,8 @@ def start_message(message):
                               'If you did it, write /start in the group and I will send you instructions for use.')
         bot.send_sticker(chat_id=message.chat.id,
                          sticker='CAACAgIAAxkBAAJOvWYSrvx7emh-acTzaw88Ufdj4gYrAAKqFQAC_QcYS-3YFx7pAAEqfDQE')
-        print(bot.get_chat(chat_id=message.chat.id).type)
         connection.close()
+
         return None
 
 
@@ -162,12 +190,14 @@ def on_off_restricts(message):
     status = bot.get_chat_member(chat_id=message.chat.id, user_id=message.from_user.id).status
     id = message.chat.id
     check_table(message)
+
     connection = sqlite3.connect('UsersDatabase.sqlite')
     cursor = connection.cursor()
+
     if status == 'creator' or status == 'administrator':
         cursor.execute(f"SELECT options FROM [{id}]")
         ban = cursor.fetchall()
-        print(ban)
+
         if ban[0][0] == 'False':
             cursor.execute(f"UPDATE [{id}] SET options = ? WHERE options = ?", ['True', 'False'])
             bot.send_message(chat_id=id, text='Punishments for using profanity are on now✅')
@@ -193,16 +223,19 @@ def set_name(message):
     check_table(message)
     msg = message.text
     msg = msg[6::]
-    print(msg)
+
     connection = sqlite3.connect('UsersDatabase.sqlite')
     cursor = connection.cursor()
     cursor.execute(f"SELECT name FROM [{message.chat.id}]")
+
     name = cursor.fetchall()[0][0]
-    print(name)
+
     cursor.execute(f"UPDATE [{message.chat.id}] SET name = ? WHERE name = ?", [msg, name])
     connection.commit()
     connection.close()
+
     bot.send_message(chat_id=message.chat.id, text=f'My name has been changed to {msg}.')
+
     return None
 
 
@@ -210,24 +243,32 @@ def set_name(message):
 def text(message):
     status = bot.get_chat_member(message.chat.id, message.from_user.id).status
     group_status = bot.get_chat(chat_id=message.chat.id).type
-    #если тип чата == группа
+
+    advertisement_group_id = 'place id of your group for advertisement as an integer here'
+
+    #if the chat type is a group
     if group_status == 'group' or group_status == 'supergroup':
-        #если чат для рекламы
+        #if the chat is for advertisement
         if message.chat.id == -4149141572:
             advertisement(message=message)
+
         else:
             check_table(message=message)
             message_list = message.text.split()
-            print(message_list)
+
             for i in range(len(message_list)):
                 if message_list[i] in bad_words:
                     if status == 'administrator' or status == 'creator':
                         bot.reply_to(message=message, text='Easy, buddy.')
+
                     else:
                         response = check_user(message=message)
+
                         if response is not None:
                             response_get(response=response, message=message)
+
                             return None
+
                         else:
                             response_get(response=0, message=message)
 
